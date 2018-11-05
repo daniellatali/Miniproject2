075a2c4ee8d2926b6586712dd7c6415fd936b133
daniellatali <43752104+daniellatali@users.noreply.github.com>
2018-11-04 22:41:57 -0500

    Add files via upload
    Compiled all the 10 mad libs into one project.

---
+++ CompiledMiniProjects.py
@@ -0,0 +1,485 @@
+#MiniProject1
+
+prompt = '> '
+
+print(f"Please insert 2 verbs")
+verb1 = input(prompt)
+verb2 = input(prompt)
+
+print(f"Please enter 2 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+
+print(f"Now enter a verb ending with -ed")
+verbed = input(prompt)
+
+print(f"Now enter another verb ending with -ing")
+verbing = input(prompt)
+
+print(f"Now enter a type of liquid ")
+liquid = input(prompt)
+
+print(f"Now input 3 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+noun3 = input(prompt)
+
+print(f"""
+Jack and Jill went up the {noun1},
+To {verb1} a pail of {liquid};
+Jack {verbed} down, and broke his {noun2},
+And Jill came {verbing} after.
+
+Then up Jack got and off did {verb2},
+As {adjective1} as he could caper,
+To old Dame Dob, who patched his {noun2}
+With {noun3} and {adjective2} paper.
+""")
+
+#MiniProject2
+
+prompt = '> '
+
+print(f"Please insert 6 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+adjective3 = input(prompt)
+adjective4 = input(prompt)
+adjective5 = input(prompt)
+adjective6 = input(prompt)
+
+print(f"Please enter a plural noun")
+pluralnoun1 = input(prompt)
+
+print(f"Now input 2 numbers")
+num1 = input(prompt)
+num2 = input(prompt)
+
+print(f"Now enter a place")
+place = input(prompt)
+
+print(f"""I'm a {adjective1}, {adjective1} man, I'm {adjective2} and I'm in my prime
+I'm a {adjective1}, {adjective1} man, I'm {adjective2} and I'm in my prime
+I don't pick my {pluralnoun1}, I'm ready for any ol' kind
+Yes, I'm a real {adjective2} man, a brand new {num1}
+Yes, I'm a real {adjective2} man, a brand new {num1}
+Well, I'm {adjective3}, I'm {adjective4}, I'm practically much alive
+Well, I'm {num2} feet tall, I ain't no hand me down
+Yes, I'm {num2} feet tall and I ain't no hand me down
+Well, I got a gal in {place} who calls me {adjective5} {adjective6} Brown
+""")
+
+#MiniProject3
+
+prompt = '> '
+
+print(f"Please enter your name")
+name = input(prompt)
+
+print(f"Please insert any nationality")
+nationality = input(prompt)
+
+print(f"Please enter 4 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+adjective3 = input(prompt)
+adjective4 = input(prompt)
+
+print(f"Now input 3 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+noun3 = input(prompt)
+
+print(f"Now enter a plural noun")
+pluralnoun = input(prompt)
+
+print(f"Enter two types of foods")
+food1 = input(prompt)
+food2 = input(prompt)
+
+print(f"Please enter 2 sets of numbers")
+number1 = input(prompt)
+number2 = input(prompt)
+
+print(f"Finally, input any kind of shapes")
+shapes = input(prompt)
+
+print(f"""Pizza was invented by a {adjective1} {nationality} chef named {name}.
+To make a pizza, you need to take a lump of {noun1}, and make a thin, round {adjective2} {noun2}.
+Then you cover it with {adjective3} sauce, {adjective4} cheese, and fresh chopped {pluralnoun}.
+Next you have to bake it in a very hot {noun3}.
+When it is done, cut it into {number1} {shapes}.
+Some kids like {food1} pizza the best, but my favorite is the {food2} pizza.
+If I could, I would eat pizza {number2} times a day!
+""")
+
+#MiniProject4
+
+prompt = '> '
+
+print(f"Please insert a color")
+color = input(prompt)
+
+print(f"Please enter 5 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+adjective3 = input(prompt)
+adjective4 = input(prompt)
+
+print(f"Now input a noun")
+noun = input(prompt)
+
+print(f"Enter a body part in plural form")
+body = input(prompt)
+
+print(f"Now enter another body part in singular form")
+body2 = input(prompt)
+
+print(f"Now enter an animal in plural form")
+animal = input(prompt)
+
+print(f"""The {color} Dragon has {adjective1} {body} and a {body2} shaped like a {noun}.
+It loves to eat {animal}, although it will feast on nearly anything. It is {adjective2} and {adjective3}.
+You must be {adjective4} around it, or you may end up as it's meal!
+""")
+
+#MiniProject5
+
+prompt = '> '
+
+print(f"Please insert a place")
+place = input(prompt)
+
+print(f"Please enter 2 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+
+print(f"Now input 2 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+
+print(f"Now input 2 plural nouns")
+pluralnoun1 = input(prompt)
+pluralnoun2 = input(prompt)
+
+print(f"Now insert two verbs")
+verb1 = input(prompt)
+verb2 = input(prompt)
+
+print(f"Now insert any number")
+number = input(prompt)
+
+print(f"Finally, input any body part")
+body = input(prompt)
+
+
+print(f"""Two {pluralnoun1}, both alike in dignity,
+In fair {place}, where we lay our scene,
+From ancient {noun1} break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross`d {pluralnoun2} take their life;
+Whole misadventured piteous overthrows
+Do with their {noun2} bury their parents` strife.
+The fearful passage of their {adjective1} love,
+And the continuance of their parents` rage,
+Which, but their children`s end, nought could {verb1} ,
+Is now the {number} hours` traffic of our stage;
+The which if you with {adjective2} {body} attend,
+What here shall {verb2}, our toil shall strive to mend.
+""")
+
+#MiniProject6
+
+prompt = '> '
+
+print(f"Please insert a female name")
+name = input(prompt)
+
+print(f"Please enter any place")
+place = input(prompt)
+
+print(f"Insert any color")
+color = input(prompt)
+
+print(f"Please enter 2 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+
+print(f"Now input 4 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+noun3 = input(prompt)
+noun4 = input(prompt)
+
+print(f"Now add 4 plural nouns")
+pluralnoun1 = input(prompt)
+pluralnoun2 = input(prompt)
+pluralnoun3 = input(prompt)
+pluralnoun4 = input(prompt)
+
+print(f"Now insert 2 types of food")
+food1 = input(prompt)
+food2 = input(prompt)
+
+print(f"Now input 5 verbs")
+verb1 = input(prompt)
+verb2 = input(prompt)
+verb3 = input(prompt)
+verb4 = input(prompt)
+verb5 = input(prompt)
+
+print(f"Input one verb ending with an s ")
+verbs = input(prompt)
+
+print(f"Now add an adverb")
+adverb = input(prompt)
+
+print(f"Input a body part")
+body = input(prompt)
+
+print(f" Finally, add an article of clothing in plural form")
+clothing = input(prompt)
+
+print(f"""Picture yourself in a {noun1} on a river,
+With {food1} trees and {food2} skies
+Somebody calls you, you {verb1} quite {adverb},
+A girl with {adjective1} eyes.
+
+Cellophane {pluralnoun1} of {color} and green,
+{verb2} over your head.
+{verb3} for the girl with the {noun2} in her eyes,
+And she`s gone.
+
+{name} in the sky with {pluralnoun2}...
+{name} in the sky with {pluralnoun2}...
+{name} in the sky with {pluralnoun2}...
+
+Follow her down to a {noun3} by a fountain
+Where rocking horse {pluralnoun3} eat {noun4} pies,
+Everyone {verbs} as you {verb4} past the flowers,
+That {verb5} so incredibly high.
+
+Newspaper {pluralnoun4} appear on the shore,
+Waiting to take you away.
+Climb in the back with your {body} in the clouds,
+And you`re gone.
+
+Picture yourself on a train in a {place},
+With {adjective2} porters with looking glass {clothing},
+Suddenly someone is there at the turnstile,
+The girl with {adjective1} eyes.
+
+{name} in the sky with {pluralnoun2}...
+{name} in the sky with {pluralnoun2}...
+{name} in the sky with {pluralnoun2}...
+""")
+
+#MiniProject7
+
+prompt = '> '
+
+print(f"Please insert a female name")
+name = input(prompt)
+
+print(f"Please enter 2 celebrities")
+famous1 = input(prompt)
+famous2 = input(prompt)
+
+print(f"Please insert a nationality")
+nationality = input(prompt)
+
+print(f"Please enter 2 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+
+print(f"Now input 3 plural nouns")
+pluralnoun1 = input(prompt)
+pluralnoun2 = input(prompt)
+pluralnoun3 = input(prompt)
+
+print(f"Now input 2 singular nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+
+print(f"Now input a verb ending with -ed ")
+verb = input(prompt)
+
+print(f"Now input an occupation")
+occupation = input(prompt)
+
+print(f"Now input a type of liquid")
+liquid = input(prompt)
+
+print(f"Now input any number")
+number = input(prompt)
+
+print(f"Now enter a place")
+place = input(prompt)
+
+print(f"""
+I enjoy long, {adjective1} walks on the beach, getting {verb} in the rain and serendipitous encounters with {pluralnoun1}.
+I really like piña coladas mixed with {liquid}, and romantic, candle-lit {pluralnoun2}.
+I am well-read from Dr. Seuss to {famous1}.
+I travel frequently, especially to {place}, when I am not busy with work. (I am a {occupation}.)
+I am looking for {noun1} and beauty in the form of a {nationality} goddess.
+She should have the physique of {famous2} and the {noun2} of {name}.
+I would prefer if she knew how to cook, clean, and wash my {pluralnoun3}.
+I know I’m not very attractive in my picture, but it was taken {number} days ago, and I have since become more {adjective2}.
+""")
+
+#MiniProject8
+
+prompt = '> '
+
+print(f"Please insert 2 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+
+print(f"Please enter any adjective")
+adjective1 = input(prompt)
+
+print(f"Now input 5 verbs")
+verb1 = input(prompt)
+verb2 = input(prompt)
+verb3 = input(prompt)
+verb4 = input(prompt)
+verb5 = input(prompt)
+
+print(f"Now enter any feeling or emotion")
+feeling = input(prompt)
+
+print(f"Finally, input any part of the body")
+body = input(prompt)
+
+print(f"""
+What would you think if I {verb1} out of tune?
+Would you {verb2} up and {verb3} out on me?
+Lend me your {body} and I'll {verb4} you a song
+And I'll try not to {verb4} out of key
+Oh, I get by with a {adjective1} help from my {noun1}
+Mm, I get high with a {adjective1} help from my {noun1}
+Mm, gonna try with a {adjective1} help from my {noun1}
+What do I do when my {noun2} is away?
+Does it worry you to be alone?
+How do I feel by the end of the day?
+Are you {feeling} because you're on your own?
+No, I get by with a {adjective1} help from my {noun1}
+Mm, get high with a {adjective1} help from my {noun1}
+Mm, gonna try with a {adjective1} help from my {noun1}
+Do you need anybody?
+I need somebody to {verb5}
+Could it be anybody?
+I want somebody to {verb5}.
+""")
+
+#MiniProject9
+
+prompt = '> '
+
+print(f"Please insert 5 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+noun3 = input(prompt)
+noun4 = input(prompt)
+noun5 = input(prompt)
+
+print(f"Please enter 4 plural nouns")
+pluralnoun1 = input(prompt)
+pluralnoun2 = input(prompt)
+pluralnoun3 = input(prompt)
+pluralnoun4 = input(prompt)
+
+print(f"Please enter 3 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+adjective3 = input(prompt)
+
+print(f"Now input a verb")
+verb1 = input(prompt)
+
+print(f"Now enter a type of food")
+food1 = input(prompt)
+
+print(f"""
+If you like to talk to {pluralnoun1}
+If a {noun1} can make you {verb1}
+If you like to waltz with {pluralnoun2}
+Up and down the produce {noun2}...
+Have we got a {noun3} for you!
+
+{food1}Tales, {food1}Tales, {food1}Tales, {food1}Tales,
+{food1}Tales, {food1}Tales, {food1}Tales, {food1}Tales!
+
+Broccoli! {pluralnoun3}! Gotta be
+{food1}Tales!
+
+{adjective1} beans! Collard {pluralnoun4}! {adjective2} {noun4}!
+{food1}Tales!
+
+Cauliflower! {adjective3} and sour! Half an hour!
+{food1}Tales!
+
+There`s never-ever-ever-ever-ever been a {noun3} like {food1}Tales!
+There`s never-ever-ever-ever-ever been a {noun3} like {food1}Tales!
+It`s {noun5} for {food1}Tales!
+""")
+
+#MiniProject10
+
+prompt = '> '
+
+print(f"Please insert 5 nouns")
+noun1 = input(prompt)
+noun2 = input(prompt)
+noun3 = input(prompt)
+noun4 = input(prompt)
+noun5 = input(prompt)
+
+print(f"Please enter 3 adjectives")
+adjective1 = input(prompt)
+adjective2 = input(prompt)
+adjective3 = input(prompt)
+
+print(f"Now input 3 plural nouns")
+pluralnoun1 = input(prompt)
+pluralnoun2 = input(prompt)
+pluralnoun3 = input(prompt)
+
+print(f"Now insert a verb")
+verb1 = input(prompt)
+
+print(f"Now insert another verb but ending with -ing")
+verbing = input(prompt)
+
+print(f"Now input 2 occupations")
+occ1 = input(prompt)
+occ2 = input(prompt)
+
+print(f"Now insert 2 school subjects")
+subject1 = input(prompt)
+subject2 = input(prompt)
+
+print(f"Finally, enter any part of the body")
+body = input(prompt)
+
+print(f"""
+The following has been quotes by Albert Einstein himself:
+(Don't look this up online trust us this is 100% factual and real.)
+
+"Any intelligent fool can make things {adjective1}, more {adjective2}, and more violent.
+It takes a touch of genius -- and a lot of {pluralnoun1} -- to move in the opposite {noun1}."
+
+"Reality is merely a {noun2}, albeit a very {adjective3} one."
+
+"Great {pluralnoun2} have often encountered violent opposition from weak {pluralnoun3}."
+
+"Technological progress is like a {noun3} in the hands of a pathological {occ1}."
+
+"No, this {noun4} won`t work... How on earth are you ever going to {verb1} in terms
+of {subject1} and {subject2} so important a biological phenomenon as {adjective3} love?"
+
+"The release of {noun5} power has changed everything except our way
+of {verbing}... the solution to this problem lies in the {body} of mankind.
+If only I had known, I should have become a {occ2}."
+""")
